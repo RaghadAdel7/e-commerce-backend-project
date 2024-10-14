f5e9d5c97d929148bc0534cc6af9dc1e4db4e704
TalalMAlqarni <147104185+TalalMAlqarni@users.noreply.github.com>
2024-10-14 19:18:58 +0300

    Add files via upload

---
+++ src/Controllers/UsersController.cs
@@ -0,0 +1,81 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using src.Entity;
+using Microsoft.AspNetCore.Mvc;
+using src.Utils;
+using src.Services.user;
+using static src.DTO.UserDTO;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using static src.Entity.User;
+
+namespace src.Controllers
+{   [ApiController]
+    [Route("api/v1/[controller]")]
+    public class UsersController  : ControllerBase
+    {
+        protected readonly IUserService _userService;
+
+        public UsersController(IUserService service)
+        {
+            _userService = service;
+        }
+        // create user
+        [HttpPost]
+        public async Task<ActionResult<UserReadDto>> CreateOne(UserCreateDto createDto)
+        {
+            var userCreated = await _userService.CreateOneAsync(createDto);
+            return Ok(userCreated);
+        }
+        
+        // log in
+        [HttpPost("signIn")]
+        public async Task<ActionResult<string>> SignInUser([FromBody] UserCreateDto createDto)
+        {
+            var token = await _userService.SignInAsync(createDto);
+            return Ok(token);
+        }
+        [HttpGet]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<List<UserReadDto>>> GetAllUsers()
+        {
+            var userList = await _userService.GetAllAsync();
+           
+                return Ok(userList);
+            
+        }
+        [HttpGet("{userId}")]
+        [Authorize(Roles = "Admin")]
+        public async Task<ActionResult<UserReadDto>> GetOrderById([FromRoute] Guid userId)
+        {
+            var foundUser = await _userService.GetByIdAsync(userId);
+            if (foundUser == null)
+            {
+                throw CustomException.BadRequest("User not found");
+            }
+            return Ok(foundUser);
+        }
+        [HttpDelete("{userId}")]
+        public async Task<ActionResult> DeleteUser(Guid userId)
+        {
+            var foundUser = await _userService.GetByIdAsync(userId);
+            if (foundUser == null)
+                throw CustomException.UnAuthorized($"user with {userId}  doesnt exist");
+
+            var isDeleted = await _userService.DeleteOneAsync(userId);
+            return isDeleted ? Ok("user deleted seccsufully") : StatusCode(500);
+        }
+         
+        [HttpPut("{userId}")]
+        public async Task<ActionResult<UserReadDto>> UpdateUser(Guid userId, UserUpdateDto updateDto)
+        {
+            var userRead = await _userService.UpdateOneAsync(userId, updateDto);
+            return Ok($"{userRead} Updated seccussfuly");
+        }
+        
+    }
+
+    
+}
---
+++ src/Program.cs
@@ -0,0 +1,127 @@
+using Microsoft.EntityFrameworkCore;
+using Npgsql;
+using src.Database;
+using src.Repository;
+using src.Services.Category;
+using src.Services.Payment;
+using src.Services.product;
+using src.Services.SubCategory;
+using src.Services.user;
+using src.Services;
+using src.Utils;
+using src.Services.cart;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using src.Middlewares;
+using static src.Entity.User;
+using src.Services.review;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+using src.Services.Coupon;
+
+
+var builder = WebApplication.CreateBuilder(args);
+//connect to database
+var dataSourceBuilder = new NpgsqlDataSourceBuilder(builder.Configuration.GetConnectionString("Local"));
+dataSourceBuilder.MapEnum<UserRole>();
+
+builder.Services.AddDbContext<DatabaseContext>(options =>
+{
+    options.UseNpgsql(dataSourceBuilder.Build());
+    options.ConfigureWarnings(x => x.Ignore(CoreEventId.ManyServiceProvidersCreatedWarning));
+}
+);
+// add utomapper
+builder.Services.AddAutoMapper(typeof(MapperProfile).Assembly);
+// add DI service
+builder
+    .Services.AddScoped<IUserService, UserService>().AddScoped<UserRepository, UserRepository>()
+    .AddScoped<IOrderService, OrderService>().AddScoped<OrderRepository, OrderRepository>()
+    .AddScoped<IProductService, ProductService>().AddScoped<ProductRepository, ProductRepository>()
+    .AddScoped<ICategoryService, CategoryService>().AddScoped<CategoryRepository, CategoryRepository>()
+    .AddScoped<ISubCategoryService, SubCategoryService>().AddScoped<SubCategoryRepository, SubCategoryRepository>()
+    .AddScoped<IPaymentService, PaymentService>().AddScoped<PaymentRepository, PaymentRepository>()
+    .AddScoped<ICartService, CartService>().AddScoped<CartRepository, CartRepository>()
+    .AddScoped<IReviewService, ReviewService>().AddScoped<ReviewRepository, ReviewRepository>()
+    .AddScoped<ICouponService, CouponService>().AddScoped<CouponRepository, CouponRepository>();
+
+builder.Services
+.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
+//auth for admin
+// role
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminOnly", policy => policy.RequireRole("Admin"));
+});
+
+
+//Add controllers
+builder.Services.AddControllers();
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// for deployment part:
+app.UseRouting();
+app.MapGet("/" , ()=> "Server is running");
+//
+// test database connection
+using (var scope = app.Services.CreateScope())
+{
+    var context = scope.ServiceProvider.GetRequiredService<DatabaseContext>();
+    try
+    {
+        if (context.Database.CanConnect())
+        {
+            Console.WriteLine("Database connection successful");
+        }
+        else
+        {
+            Console.WriteLine("Database connection failed");
+        }
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine(ex.Message);
+    }
+}
+// add middleware 
+app.UseMiddleware<LoggingMiddleware>();
+app.UseMiddleware<ErrorHandlerMiddleware>();
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.MapControllers();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+
+app.UseHttpsRedirection();
+
+app.Run();
